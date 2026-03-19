57965e770765d7e54dec937cb3212b43f7e22973
Jack <j72658670@gmail.com>
2026-03-19 18:24:01 +0100

    Add additional tests for root and 404 routes

--- app.test.js
+++ app.test.js
@@ -4,7 +4,6 @@ const app = require('./app');
 describe ('GET /users', ()=> {
 	it('should return a list of users as JSON', async () => {
 	const response = await request(app).get('/users');
-
 	expect(response.statusCode).toBe(200);
 	expect(response.body).toEqual([
 	 {id: 1, name: 'Jack' },
@@ -13,5 +12,17 @@ describe ('GET /users', ()=> {
        });
      });
 
+describe('GET /', () => {
+ test('should return Hello Dev Ops World', async() => {
+  const res = await request(app).get('/');
+  expect(res.text).toContain('Hello DevOps World');
+  expect(res.statusCode).toBe(200);
+ });
+});
 
-
+describe('GET /notfound', () => {
+  test('should return 404 for unknown route', async () => {
+    const res = await request(app).get('/notfound');
+    expect(res.statusCode).toBe(404);
+  });
+});
