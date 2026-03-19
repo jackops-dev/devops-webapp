9ebefd74eedb081be3b905982f38ce3d3275fcac
Jack <j72658670@gmail.com>
2026-03-19 17:45:03 +0100

    Update home route message in feature branch

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-	res.send('Hello DevOps World');
+	res.send('Hello DevOps World from feature branch 🚀');
 	});
 
 app.get('/users', (req, res) => {
